ef28e5c956837b9c63d1ddb069ac275be14146a4
Miquel van Smoorenburg <mikevs@xs4all.net>
2018-04-26 16:59:49 +0200

    implement precommit-cache

--- storage/src/history/mod.rs
+++ storage/src/history/mod.rs
@@ -21,7 +21,8 @@ pub(crate) trait HistBackend: Send + Sync {
 
 /// History database functionality.
 pub struct History {
-    inner:  RwLock<InnerHistory>,
+    inner:      RwLock<InnerHistory>,
+    pc_cache:   RwLock<HCache>,
 }
 
 struct InnerHistory {
@@ -51,20 +52,21 @@ pub enum HistStatus {
 impl History {
     /// Open history file.
     pub fn open<T: AsRef<Path>>(tp: &str, path: T) -> io::Result<History> {
-        match tp {
+        let h = match tp {
             "diablo" => {
-                let h = diablo::DHistory::open(path.as_ref())?;
-                Ok(History{
-                    inner:  RwLock::new(InnerHistory{
-                        backend:    Box::new(h),
-                        cache:      HCache::new(32000, 300),
-                    })
-                })
+                diablo::DHistory::open(path.as_ref())
             },
             s => {
                 Err(io::Error::new(io::ErrorKind::InvalidData, s.to_string()))
-            }
-        }
+            },
+        }?;
+        Ok(History{
+            inner:  RwLock::new(InnerHistory{
+                backend:    Box::new(h),
+                cache:      HCache::new(64000, 300),
+            }),
+            pc_cache:   RwLock::new(HCache::new(8000, 30)),
+        })
     }
 
     /// Find an entry in the history file.
@@ -77,6 +79,52 @@ impl History {
             return Ok(he)
         }
         inner.backend.lookup(msgid.as_ref())
+    }
+
+    /// Like lookup, but also uses the precommit-cache.
+    /// If an entry is found in the precommit-cache and it
+    /// is not too old, return it. If no entry is found at
+    /// all, put an entry into the precommit-cache.
+    pub fn check<T: AsRef<[u8]>>(&self, msgid: T) -> io::Result<HistEnt> {
+
+        let id = msgid.as_ref();
+        {
+            // check precommit-cache.
+            let p = self.pc_cache.read().unwrap();
+            let pc_cache = &*p;
+
+            let he = pc_cache.lookup(id);
+            if he.status == HistStatus::Tentative {
+                return Ok(he);
+            }
+        }
+
+        let i = self.inner.read().unwrap();
+        let inner = &*i;
+
+        // check history cache.
+        let he = inner.cache.lookup(id);
+        if he.status != HistStatus::NotFound {
+            return Ok(he)
+        }
+
+        // check history database.
+        let he = inner.backend.lookup(id)?;
+        if he.status == HistStatus::NotFound {
+
+            // not found, so insert a tentative entry into
+            // the precommit-cache.
+            let mut p = self.pc_cache.write().unwrap();
+            let pc_cache = &mut *p;
+            pc_cache.store(id, &HistEnt{
+                status:     HistStatus::Tentative,
+                time:       0,
+                head_only:  false,
+                location:   None,
+            });
+        }
+
+        Ok(he)
     }
 
     pub fn store(&self, msgid: &[u8], he: &HistEnt) -> io::Result<()> {
