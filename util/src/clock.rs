4320a3ece15c35f92b20e4933513c7f3bbc9b3b8
Miquel van Smoorenburg <mikevs@xs4all.net>
2018-09-25 13:06:46 +0200

    move clock code to util/

--- util/src/clock.rs
+++ util/src/clock.rs
@@ -10,7 +10,11 @@
 //!
 use libc::{self,clockid_t};
 
-pub use time::Timespec;
+/// Returned by clock::gettime()
+pub struct Timespec{
+    pub sec:    i64,
+    pub nsec:   i32,
+}
 
 /// Clock ID for clock::gettime, see also the manpage for the
 /// clock_gettime() systemcall.
@@ -19,7 +23,7 @@ pub enum ClockId {
     /// realtime since the Unix epoch. can jump back or forward.
     Realtime = libc::CLOCK_REALTIME as isize,
     /// like REALTIME, but with a granularity of about 1ms. 3x faster though.
-    RealtimeCourse = libc::CLOCK_REALTIME_COARSE as isize,
+    RealtimeCoarse = libc::CLOCK_REALTIME_COARSE as isize,
     /// time since some internal epoch. monotonic, no jumps.
     Monotonic = libc::CLOCK_MONOTONIC as isize,
     /// like MONOTONIC, but with a granularity of about 1ms. 3x faster though.
@@ -32,8 +36,12 @@ pub enum ClockId {
 pub enum ClockId {
     /// realtime since the Unix epoch. can jump back or forward.
     Realtime = libc::CLOCK_REALTIME as isize,
+    /// on this non-linux platform RealtimeCoarse is the same as Realtime
+    RealtimeCoarse = libc::CLOCK_REALTIME as isize,
     /// time since some internal epoch. monotonic, no jumps.
     Monotonic = libc::CLOCK_MONOTONIC as isize,
+    /// on this non-linux platform MonotonicCoarse is the same as Monotonic
+    MonotonicCoarse = libc::CLOCK_MONOTONIC as isize,
     #[doc(hidden)]
     _Placeholder = -1,
 }
@@ -49,44 +57,36 @@ pub fn gettime(clk_id: ClockId) -> Timespec {
     }
 }
 
-/// Convenience function for gettime(ClockId::RealtimeCoarse), returns ms.
-/// Called js_now() because it's basically javascript's Date.now()
-#[cfg(target_os = "linux")]
+/// Return unix time in seconds.
 #[inline(always)]
-pub fn js_now() -> u64 {
+pub fn unixtime() -> u64 {
 	let mut ts = libc::timespec{ tv_sec: 0, tv_nsec: 0 };
-	unsafe { libc::clock_gettime(ClockId::RealtimeCourse as clockid_t, &mut ts); }
-	ts.tv_sec as u64 + (ts.tv_nsec / 1000000) as u64
+	unsafe { libc::clock_gettime(ClockId::RealtimeCoarse as clockid_t, &mut ts); }
+	ts.tv_sec as u64
 }
 
-/// Convenience function for gettime(ClockId::MonotonicCoarse), returns ms.
-#[cfg(target_os = "linux")]
+/// Return unix time in milliseconds.
 #[inline(always)]
-pub fn mt_now() -> u64 {
+pub fn unixtime_ms() -> u64 {
 	let mut ts = libc::timespec{ tv_sec: 0, tv_nsec: 0 };
-	unsafe { libc::clock_gettime(ClockId::MonotonicCourse as clockid_t, &mut ts); }
-	ts.tv_sec as u64 + (ts.tv_nsec / 1000000) as u64
+	unsafe { libc::clock_gettime(ClockId::RealtimeCoarse as clockid_t, &mut ts); }
+	1000 * ts.tv_sec as u64 + (ts.tv_nsec / 1000000) as u64
 }
 
-/// Convenience function for gettime(ClockId::Realtime), returns ms.
-/// Called js_now() because it's basically javascript's Date.now()
-#[cfg(not(target_os = "linux"))]
+/// Return monotonic time in seconds.
 #[inline(always)]
-pub fn js_now() -> u64 {
+pub fn monotime() -> u64 {
 	let mut ts = libc::timespec{ tv_sec: 0, tv_nsec: 0 };
-	unsafe { libc::clock_gettime(ClockId::Realtime as clockid_t, &mut ts); }
-	ts.tv_sec as u64 + (ts.tv_nsec / 1000000) as u64
+	unsafe { libc::clock_gettime(ClockId::MonotonicCoarse as clockid_t, &mut ts); }
+	ts.tv_sec as u64
 }
-
-/// Convenience function for gettime(ClockId::Monotonic), returns ms.
-#[cfg(not(target_os = "linux"))]
+/// Return monotonic time in milliseconds.
 #[inline(always)]
-pub fn mt_now() -> u64 {
+pub fn monotime_ms() -> u64 {
 	let mut ts = libc::timespec{ tv_sec: 0, tv_nsec: 0 };
-	unsafe { libc::clock_gettime(ClockId::Monotonic as clockid_t, &mut ts); }
-	ts.tv_sec as u64 + (ts.tv_nsec / 1000000) as u64
+	unsafe { libc::clock_gettime(ClockId::MonotonicCoarse as clockid_t, &mut ts); }
+	1000 * ts.tv_sec as u64 + (ts.tv_nsec / 1000000) as u64
 }
-
 
 #[cfg(test)]
 mod tests {
--- util/src/lib.rs
+++ util/src/lib.rs
@@ -6,20 +6,17 @@ extern crate libc;
 extern crate regex;
 extern crate serde;
 
-use std::time::SystemTime;
-
+mod clock;
 mod dateparser;
 mod de;
 mod hostname;
 mod wildmat_fn;
 mod wildmat;
 
+pub use clock::*;
 pub use dateparser::*;
 pub use de::*;
 pub use hostname::*;
 pub use wildmat_fn::*;
 pub use wildmat::*;
 
-pub fn unixtime() -> u64 {
-    SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()
-}
