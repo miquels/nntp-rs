175e472a49093928bb0cd5e314afe01ff64bbe90
Miquel van Smoorenburg <mikevs@xs4all.net>
2018-10-09 18:20:13 +0200

    add dhash implementation to util

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,6 +3,7 @@
 mod clock;
 mod dateparser;
 mod de;
+mod dhash;
 mod hostname;
 mod wildmat_fn;
 mod wildmat;
@@ -10,6 +11,7 @@ mod wildmat;
 pub use self::clock::*;
 pub use self::dateparser::*;
 pub use self::de::*;
+pub use self::dhash::*;
 pub use self::hostname::*;
 pub use self::wildmat_fn::*;
 pub use self::wildmat::*;
