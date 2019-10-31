baf7796aba5bc2fa5d83b07ef4a133c9e114b02a
Miquel van Smoorenburg <mike@langeraar.net>
2019-10-31 19:11:16 +0100

    add Criterion benchmark thing

---
+++ benches/clocksources.rs
@@ -0,0 +1,45 @@
+use std::time::{Instant, SystemTime};
+
+use libc;
+use time;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn bench_timesources(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Time sources");
+    group.bench_function("SystemTime::now()", |b| b.iter(|| {
+        SystemTime::UNIX_EPOCH.elapsed().unwrap().as_secs();
+    }));
+    group.bench_function("Instant::now()", |b| b.iter(|| {
+        Instant::now();
+    }));
+    group.bench_function("time::now_utc()", |b| b.iter(|| {
+        time::now_utc();
+    }));
+    group.bench_function("libc::CLOCK_REALTIME", |b| b.iter(|| {
+        let mut ts = libc::timespec{ tv_sec: 0, tv_nsec: 0 };
+        unsafe { libc::clock_gettime(libc::CLOCK_REALTIME, &mut ts); }
+    }));
+    group.bench_function("libc::CLOCK_REALTIME_COARSE", |b| b.iter(|| {
+        let mut ts = libc::timespec{ tv_sec: 0, tv_nsec: 0 };
+        unsafe { libc::clock_gettime(libc::CLOCK_REALTIME_COARSE, &mut ts); }
+    }));
+    group.bench_function("libc::CLOCK_MONOTONIC", |b| b.iter(|| {
+        let mut ts = libc::timespec{ tv_sec: 0, tv_nsec: 0 };
+        unsafe { libc::clock_gettime(libc::CLOCK_MONOTONIC, &mut ts); }
+    }));
+    group.bench_function("libc::CLOCK_MONOTONIC_COARSE", |b| b.iter(|| {
+        let mut ts = libc::timespec{ tv_sec: 0, tv_nsec: 0 };
+        unsafe { libc::clock_gettime(libc::CLOCK_MONOTONIC_COARSE, &mut ts); }
+    }));
+    group.bench_function("libc::gettimeofday", |b| b.iter(|| {
+        let mut ts = libc::timeval{ tv_sec: 0, tv_usec: 0 };
+        let nullptr : *mut libc::timezone = std::ptr::null_mut();
+        unsafe { libc::gettimeofday(&mut ts, nullptr); }
+    }));
+    group.finish();
+}
+
+criterion_group!(benches, bench_timesources);
+criterion_main!(benches);
+
